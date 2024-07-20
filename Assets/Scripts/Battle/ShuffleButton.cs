4a460da1174c5d7ceccf5444da5acbbf00ff2592
LWCoding <DiamondMob01@gmail.com>
2024-07-19 17:28:56 -0700

    Removed unnecessary health handler, standardized cursor size for WebGL build (max size = 32)

--- Assets/Scripts/Battle/ShuffleButton.cs
+++ Assets/Scripts/Battle/ShuffleButton.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-[RequireComponent(typeof(HealthHandler))]
 [RequireComponent(typeof(PointerCursorOnHover))]
 public class ShuffleButton : MonoBehaviour
 {
