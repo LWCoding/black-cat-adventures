0ce72b95455b5cd938b86d7b7667d0becb9a7f45
LWCoding <DiamondMob01@gmail.com>
2024-09-07 00:29:35 -0700

    Erase save button doesn't show up on the webgl build

--- Assets/Scripts/SaveLoad/EraseSave.cs
+++ Assets/Scripts/SaveLoad/EraseSave.cs
@@ -5,7 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class EraseSave : MonoBehaviour
 {
-    
+
+#if UNITY_WEBGL && !UNITY_EDITOR
+    private void Awake()
+    {
+        Debug.Log("WebGL build, deleting save button");
+        Destroy(gameObject);
+    }
+#endif
+
     /// <summary>
     /// Delete the save file AND reset the player's information.
     /// </summary>
